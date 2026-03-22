356d71a60a97906d11990bc9712c0bb5d9e2a9dc
mohamedmas2 <55569264+mohamedmas2@users.noreply.github.com>
2026-03-22 05:11:34 +0200

    Delete marketing/js directory

--- marketing/js/global.js
+++
@@ -1,8 +0,0 @@
-const backBtn = document.createElement("a");
-
-backBtn.href = "../index.html";
-backBtn.innerText = "← الرئيسية";
-
-backBtn.className = "fixed top-6 left-6 bg-white border border-sky-200 text-sky-700 px-4 py-2 rounded-full shadow hover:bg-sky-50 transition z-50";
-
-document.body.appendChild(backBtn);
